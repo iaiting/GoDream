2bb9d044f9d33bd09170367432a7f8d1d71a16b6
iaiting <iaiting@aliyun.com>
2018-03-24 12:49:03 +0800

    (*) 优化GoDream工程

--- study-books/Go语言编程/chap02.go
+++ study-books/Go语言编程/chap02.go
@@ -20,17 +20,99 @@ import (
 
 // =============================================================================
 //
-// 以下为自行练习例子
+// 以下为自行练习的例子
 //
 // =============================================================================
 
-func chap02_01() {
-	fmt.Println("sssssssss")
+// 变量定义
+// =============================================================================
+func chap0201_01() {
+	var int_a int
+	int_a = 123
+
+	var string_a string
+	string_a = "abc123"
+
+	var aint_a [10]int
+	aint_a = [10]int{1, 2, 3}
+
+	var sint_a []int
+	sint_a = []int{3, 2, 1}
+
+	var struct_a struct {
+		int_a    int
+		string_a string
+		aint_a   [10]int
+		sint_a   []int
+	}
+
+	var pint_a *int
+	pint_a = &int_a
+
+	var msringint_a map[string]int
+	// msringint_a['name'] = "abc123"
+
+	// var func_a func(a int) int
+
+	var (
+		int_b    int
+		string_b string
+	)
+	int_b = 123
+	string_b = "abc"
+
+	// 打印变量
+	fmt.Println(int_a)
+	fmt.Println(string_a)
+	fmt.Println(aint_a)
+	fmt.Println(sint_a)
+	fmt.Println(struct_a)
+	fmt.Println(pint_a)
+	fmt.Println(msringint_a)
+	// fmt.Println(func_a(1))
+
+	fmt.Println(int_b)
+	fmt.Println(string_b)
+}
+
+// 变量初始化
+// =============================================================================
+func chap0201_02() {
+	var int_a1 int = 1234567890
+	var int_a2 = 2345678901
+	int_a3 := 3456789012
+
+	fmt.Println(int_a1)
+	fmt.Println(int_a2)
+	fmt.Println(int_a3)
+}
+
+// 变量赋值
+// =========================================================================
+func chap0201_03() {
+	var int_a int
+	int_a = 1234567890
+
+	var int_b int
+	int_b = 2345678901
+
+	fmt.Println(int_a, int_b)
+
+	int_a, int_b = int_b, int_a
+	fmt.Println(int_a, int_b)
+
+}
+
+// 匿名变量
+// =============================================================================
+func chap0201_04() {
+	_, _, nickName := "Qiao", "ting", "iaiting"
+	fmt.Println(nickName)
 }
 
 // =============================================================================
 //
-// 以下为书本原生例子
+// 以下为书本原生的例子
 //
 // =============================================================================
 
