793880ff93d6dea5317164d2e5ac61e10bfd6ed1
lescpsn <lescpsn@lescpsndeMacBook-Pro.local>
2018-10-17 10:41:30 +0800

    (*) 优化工程

--- Go语言编程/chap02.go
+++ Go语言编程/chap02.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -11,7 +12,7 @@ import (
 // 第 2 章 顺序编程
 //
 ////////////////////////////////////////////////////////////////////////////////
-func chap02_t01() {
+func chap02_t00() {
 
 	simple_log("Enter:")
 
@@ -29,7 +30,7 @@ func chap02_t01() {
 }
 
 // 2.1.1 变量声明
-func chap02_t0101() {
+func chap02_t01() {
 	simple_log("Enter:")
 
 	// 整型变量声明
@@ -78,8 +79,91 @@ func chap02_t0101() {
 	simple_log("Leave:\n")
 }
 
+// 2.2 常量
+// 2.2.2 常量定义
+func chap02_t02() {
+	simple_log("Enter:")
+
+	const zero_0 = 0.0
+	const zero_1 = 0
+
+	const zero_2 = iota
+
+	const v1 = iota * 11
+
+	fmt.Println("zero_0 类型:", reflect.TypeOf(zero_0))
+	fmt.Println("zero_1 类型:", reflect.TypeOf(zero_1))
+	fmt.Println("zero_2 类型:", reflect.TypeOf(zero_2))
+	fmt.Println("v1 类型:", reflect.TypeOf(v1))
+
+	simple_log("Leave:\n")
+}
+
+// 星期枚举
+func chap02_t03() {
+	simple_log("Enter:")
+
+	const (
+		Sunday = iota
+		Monday
+		Tuesday
+		Wednesday
+		Thursday
+		Friday
+		Saturday
+		numberOfDays
+	)
+
+	fmt.Println("Sunday =", Sunday)
+
+	fmt.Println("Monday =", Monday)
+
+	fmt.Println("Tuesday =", Tuesday)
+
+	fmt.Println("Wednesday =", Wednesday)
+
+	simple_log("Leave:\n")
+
+}
+
+// 2.3 类型
+// 2.3.1 布尔类型
+func chap02_t04() {
+	simple_log("Enter:")
+
+	var b_v1 bool = true
+	var b_v2 bool = false
+	fmt.Println("b_v1 =", b_v1)
+	fmt.Println("b_v2 =", b_v2)
+
+	simple_log("Leave:\n")
+
+}
+
+// 2.3.2 整型
+func chap02_t05() {
+	simple_log("Enter:")
+
+	var i_v1 int8 = 127
+	fmt.Println("i_v1 =", i_v1)
+
+	var ui_v2 uint8 = 0xfe
+	fmt.Println("i_v1 =", ui_v2)
+
+	simple_log("Leave:\n")
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 func chap02_main() {
-	// chap02_t01() //
-	chap02_t0101()
+
+	chap02_t01()
+
+	chap02_t02()
+
+	chap02_t03()
+
+	chap02_t04()
+
+	chap02_t05()
+
 }
